bc5872b9c5071e7fa67fca4f4c3e2a5805ee5cd7
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-07 14:10:20 +0300

    add Example002

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 2. Принимает на вход 3 числа и выдает максимальное из этих чисел
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+int value_1 = Prompt("Введите первое число ->");
+int value_2 = Prompt("Введите второе число ->");
+int value_3 = Prompt("Введите третье число ->");
+
+int max = value_1;
+
+if (value_2 > max)
+{
+    max = value_2;
+}
+if (value_3 > max)
+{
+    max = value_3;
+}
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
