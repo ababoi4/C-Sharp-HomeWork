b75927dcec8cdd7115b7e537048d3f2e5100b3cd
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-07 13:37:21 +0300

    Add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача № 3 На вход принимает число и вадает, является ли это число чётным
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+
+int i;
+i = Prompt("Введите число > ");
+if (i % 2 == 0) 
+{
+    System.Console.WriteLine("Yes");
+}
+else 
+{
+    System.Console.WriteLine("No");
+}
