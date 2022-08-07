90b1220768c33882de60b13041ea3b73315e5b1b
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-07 13:20:55 +0300

    Add Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача № 4 На вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+
+int N;
+
+N = Prompt("Введите число > ");
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
